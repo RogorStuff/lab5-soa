8cbd83708bc740c576649b901e68404c0079f4e2
RogorStuff <lacruzrogelio@gmail.com>
2020-12-09 16:54:59 +0100

    String splicer added

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -1,5 +1,7 @@
 package soa.eip;
 
+import org.apache.camel.Processor;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +15,30 @@ public class Router extends RouteBuilder {
     from(DIRECT_URI)
       .log("Body contains \"${body}\"")
       .log("Searching twitter for \"${body}\"!")
-      .toD("twitter-search:${body}")
+      .choice()
+        .when(body().regex(".*max:[0-9]+.*"))
+          .process(sizedBody())
+          .toD("twitter-search:${body}?count=${header.count}")
+          .endChoice()
+        .otherwise()
+          .toD("twitter-search:${body}")
+          .endChoice()
+      .end()
       .log("Body now contains the response from twitter:\n${body}");
+  }
+
+  private Processor sizedBody() {
+    return new Processor() {
+      public void process(Exchange exchange) throws Exception {
+        String received = exchange.getIn().getBody(String.class);
+        String body = "", max = "";
+        for(String part : received.split(" ")) {
+          if(part.matches("max:[0-9]+")) max = part.split(":")[1];
+          else body += part + " ";
+        }
+        exchange.getIn().setBody(body);
+        exchange.getIn().setHeader("count", max);
+      }
+    };
   }
 }
